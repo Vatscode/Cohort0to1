9457147dd92314dc3bf8f0287449794c4f6ad39a
Vatscode <becomeharkirat1112@gmail.com>
2024-12-23 21:23:45 -0400

    Delete Week1 directory

--- Week1/numbersPractice.js
+++
@@ -1,5 +0,0 @@
-//parseInt
-
-console.log(parseInt("42"));
-console.log(parseInt("42vats")); // here the starting index should be int for it to work, or else youll get NaN.
-console.log(parseInt("42.96"));
