c4951c89c153b2bd2e2ed1afa3e00b9e2b4bd6fa
Vatscode <becomeharkirat1112@gmail.com>
2025-01-19 13:09:17 -0400

    Add files via upload

---
+++ cohort0to1/Week4/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+
+app.use(cors());
+app.get("/sum", (req, res)=>{
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+    const sum = a + b;
+    res.send(sum.toString());
+
+});
+
+app.get("/interest", (req, res)=>{
+    const principal = parseInt(req.query.principal);
+    const rate = parseInt(req.query.rate);
+    const time = parseInt(req.query.time);
+    const interest =(principal * rate * time)/ 100;
+    const total  = principal + interest;
+    res.send({
+        total: total,
+        interest: interest,
+
+    })
+
+
+});
+app.listen(8000, () => {
+    console.log('Server running on port 8000');
+});
